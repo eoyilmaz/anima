32cfdb8894c359559779afe344a1619ebfe1aaf5
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2013-06-19 12:15:33 +0300

    no real changes

--- setup.py
+++ setup.py
@@ -1,9 +1,21 @@
 # -*- coding: utf-8 -*-
 # Copyright (c) 2012-2013, Anima Tools
 # 
-# This module is part of oyProjectManager and is released under the BSD 2
-# License: http://www.opensource.org/licenses/BSD-2-Clause
-
+# This file is part of Anima Tools.
+# 
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation;
+# version 2.1 of the License.
+# 
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301 USA
 
 import os.path
 from setuptools import setup, find_packages
