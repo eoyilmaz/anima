cf31240a1f05ee7669743af1dbace365d7127214
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2014-01-13 12:13:22 +0200

    added ui scripts module for houdini

---
+++ anima/pipeline/ui/scripts/houdini.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012-2013, Anima Istanbul
+#
+# This module is part of anima-tools and is released under the BSD 2
+# License: http://www.opensource.org/licenses/BSD-2-Clause
+
+
+def version_creator():
+    """helper function to display version_creator in houdini
+    """
+    from stalker import db
+    from stalker.db import DBSession
+    DBSession.remove()
+    db.setup()
+
+    from anima.pipeline.ui import version_creator
+    from anima.pipeline.env import houdiniEnv
+    hEnv = houdiniEnv.Houdini()
+
+    import logging
+    logger = logging.getLogger('anima.pipeline.ui.version_creator')
+    logger.setLevel(logging.WARNING)
+    logger = logging.getLogger('anima.pipeline.ui.models')
+    logger.setLevel(logging.WARNING)
+
+    version_creator.UI(hEnv)
