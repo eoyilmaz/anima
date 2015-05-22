49cf5cc8d93f1b6445f82514d38a87ed7f23bd8a
Sergen Eren <anima.sergen@gmail.com>
2015-05-22 10:18:30 +0300

    Upgrade: commented out bounding box information from second input. h2a now uses single input for bounding box data

--- anima/render/arnold/h2a.py
+++ anima/render/arnold/h2a.py
@@ -62,15 +62,15 @@ class Buffer(object):
         return self.file_str.getvalue()
 
 
-def geometry2ass(**kwargs):
+def geometry2ass(path, name, min_pixel_width, mode, export_type, export_motion, **kwargs):
     """exports geometry to ass format
     """
-    ass_path = kwargs['path']
-    name = kwargs['name']
-    min_pixel_width = kwargs['min_pixel_width']
-    mode = kwargs['mode']
-    export_type = kwargs['export_type']
-    export_motion = kwargs['export_motion']
+    ass_path = path
+    name = name
+    min_pixel_width = min_pixel_width
+    mode = mode
+    export_type = export_type
+    export_motion = export_motion
 
     start_time = time.time()
 
@@ -112,6 +112,9 @@ def geometry2ass(**kwargs):
 
     print('Writing to file              : %3.3f' % (write_end - write_start))
 
+
+
+    """
     node_inputs = node.inputs()
     try:
         second_input_geo = node_inputs[1].geometry()
@@ -123,10 +126,15 @@ def geometry2ass(**kwargs):
         bounding_box = second_input_geo.intrinsicValue('bounds')
     else:
         bounding_box = node.geometry().intrinsicValue('bounds')
+    """
+
+
+    bounding_min = node.geometry().findGlobalAttrib("bound_min")
+    bounding_max = node.geometry().findGlobalAttrib("bound_max")
 
     bounding_box_info = 'bounds %s %s %s %s %s %s' % (
-        bounding_box[0], bounding_box[2], bounding_box[4],
-        bounding_box[1], bounding_box[3], bounding_box[5]
+        bounding_min[0], bounding_min[2], bounding_min[4],
+        bounding_max[1], bounding_max[3], bounding_max[5]
     )
 
     with open(asstoc_path, 'w') as asstoc_file:
