ece577d3ba50b633f1edcec02f34f1e162d6aa4b
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2017-03-17 14:38:01 +0300

    * **Fix:** Disabled ``defaultArnoldRenderOptions`` creation on Maya startup in Maya 2014.

--- anima/env/mayaEnv/config/userSetup.py
+++ anima/env/mayaEnv/config/userSetup.py
@@ -79,20 +79,22 @@ if 'ANIMA_TEST_SETUP' not in os.environ.keys():
         # create defaultArnoldRenderOptions
         # to disable autotx
         # TODO: Make this beautiful
-        try:
-            pm.PyNode('defaultArnoldRenderOptions')
-        except pm.MayaNodeError:
-            pm.createNode(
-                'aiOptions',
-                name='defaultArnoldRenderOptions'
-            )
-        finally:
-            daro = pm.PyNode('defaultArnoldRenderOptions')
+        
+        if int(pm.about(v=1)) > 2014:
             try:
-                daro.setAttr("autotx", 0)
-            except AttributeError:  # Maya2014
-                pass
-        pm.select(None)
+                pm.PyNode('defaultArnoldRenderOptions')
+            except pm.MayaNodeError:
+                pm.createNode(
+                    'aiOptions',
+                    name='defaultArnoldRenderOptions'
+                )
+            finally:
+                daro = pm.PyNode('defaultArnoldRenderOptions')
+                try:
+                    daro.setAttr("autotx", 0)
+                except AttributeError:  # Maya2014
+                    pass
+            pm.select(None)
 
     mayautils.executeDeferred(load_arnold)
     mayautils.executeDeferred(__pluginLoader, 'AbcExport')
