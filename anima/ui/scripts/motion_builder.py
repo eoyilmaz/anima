0a6fb88ca755c466820eff4ee3f20d4fe1e3f4f3
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2017-07-12 18:41:55 +0300

    * **New:** Added basic support for MotionBuilder.

---
+++ anima/ui/scripts/motion_builder.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2012-2017, Anima Istanbul
+#
+# This module is part of anima-tools and is released under the BSD 2
+# License: http://www.opensource.org/licenses/BSD-2-Clause
+import logging
+from anima import logger
+
+
+def set_qt_lib():
+    """sets the Qt lib according to the motion builder version
+    """
+    from anima import ui
+    ui.SET_PYSIDE()
+
+
+def version_creator(logging_level=logging.WARNING):
+    """Helper function for version_creator UI for MotionBuilder
+    """
+    # connect to db
+    from anima.utils import do_db_setup
+    do_db_setup()
+
+    # set Qt library
+    set_qt_lib()
+
+    from anima.ui import version_creator
+    from anima.env import motion_builder
+    mb = motion_builder.MotionBuilder()
+
+    logger.setLevel(logging_level)
+
+    version_creator.UI(environment=mb)
