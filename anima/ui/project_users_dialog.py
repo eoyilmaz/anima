170eff8f62e5045200c261627f4603c0aba4c477
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2018-01-26 01:52:01 +0300

    * [MAIN] Updated copyright information of newly created files, to reflect the lead developer's working condition.

--- anima/ui/project_users_dialog.py
+++ anima/ui/project_users_dialog.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2017, Anima Istanbul
+# Copyright (c) 2017-2018, Erkan Ozgur Yilmaz
 #
 # This module is part of anima-tools and is released under the BSD 2
 # License: http://www.opensource.org/licenses/BSD-2-Clause
