de91d76763024a66857f14d9abcef2c60ba124af
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-03-28 16:19:13 +0300

    * [Maya] Added ``RSProxy Data Importer`` to Toolbox's General tab. This is a nice tool that allows one to use Houdini as an editor for scattering RSProxies around and then re-create the data in Maya.

--- anima/env/mayaEnv/toolbox.py
+++ anima/env/mayaEnv/toolbox.py
@@ -289,6 +289,15 @@ def UI():
 
             color.change()
             pm.button(
+                'import_rsproxy_data_from_houdini_button',
+                l='Import RSProxy Data From Houdini',
+                c=RepeatedCallback(General.rsproxy_data_importer),
+                ann=General.rsproxy_data_importer.__doc__,
+                bgc=color.color
+            )
+
+            color.change()
+            pm.button(
                 'generate_thumbnail_button',
                 l='Generate Thumbnail',
                 c=RepeatedCallback(General.generate_thumbnail),
@@ -2484,6 +2493,114 @@ class General(object):
         import re
         [node.rename(re.sub('[\d]+', '#', node.name()))
          for node in pm.selected()]
+
+    @classmethod
+    def rsproxy_data_importer(cls):
+        """Imports RsProxy data from Houdini
+
+        Required point attributes
+            pos
+            rot
+            sca
+        """
+
+        class DataManager(object):
+            """Manages data
+            """
+
+            def __init__(self):
+                self.data = []
+
+            def load(self, path):
+                import json
+                with open(path, "r") as f:
+                    data = json.load(f)
+
+                for i in range(len(data['instancefile'])):
+                    data_obj = DataObject()
+                    self.data.append(data_obj)
+
+                    data_obj.pos = data['pos'][i]
+                    data_obj.rot = data['rot'][i]
+                    data_obj.sca = data['sca'][i]
+                    data_obj.hierarchy_name = data['hierarchy_name'][i]
+                    data_obj.instancefile = data['instancefile'][i]
+                    data_obj.node_name = data['node_name'][i]
+
+            def create(self):
+                """creates all nodes
+                """
+                for d in self.data:
+                    d.create()
+
+        class DataObject(object):
+            """Holds raw data
+            """
+
+            def __init__(self):
+                self.pos = None
+                self.rot = None
+                self.sca = None
+                self.instacefile = ''
+                self.node_name = ''
+
+                self.transform_node = None
+                self.shape_node = None
+                self.rs_proxy_node = None
+                self.hierarchy_name = None
+
+            def get_parent(self):
+                """gets the parent node or creates one
+                """
+                parent_node_name = self.hierarchy_name
+                nodes_with_name = pm.ls(parent_node_name)
+                parent_node = None
+                if nodes_with_name:
+                    parent_node = nodes_with_name[0]
+
+                if not parent_node:
+                    # create one
+                    parent_node = pm.nt.Transform(name=parent_node_name)
+
+                return parent_node
+
+            def create(self):
+                """creates Maya objects
+                """
+                self.parent_node = self.get_parent()
+                self.shape_node = pm.nt.Mesh(name='%sShape' % self.node_name)
+                self.transform_node = self.shape_node.getParent()
+                self.transform_node.rename(self.node_name)
+
+                self.rs_proxy_node = \
+                    pm.nt.RedshiftProxyMesh(name='%sRsProxy' % self.node_name)
+                self.rs_proxy_node.outMesh >> self.shape_node.inMesh
+                self.rs_proxy_node.fileName.set(self.instancefile)
+
+                # self.transform_node.t.set(self.pos)
+                # self.transform_node.r.set(self.rot)
+                # self.transform_node.s.set([self.sca, self.sca, self.sca])
+                self.parent_node.t.set(self.pos)
+                self.parent_node.r.set(self.rot)
+                self.parent_node.s.set([self.sca, self.sca, self.sca])
+
+                # assign default shader
+                lambert1 = pm.ls('lambert1')[0]
+                lambert1_shading_group = \
+                    lambert1.outputs(type='shadingEngine')[0]
+                pm.sets(lambert1_shading_group, fe=self.transform_node)
+
+                pm.parent(self.transform_node, self.parent_node, r=1)
+
+        import os
+        import tempfile
+        path = os.path.join(
+            tempfile.gettempdir(),
+            'rsproxy_info.json'
+        )
+        data_man = DataManager()
+        data_man.load(path)
+        data_man.create()
 
 
 class Previs(object):
