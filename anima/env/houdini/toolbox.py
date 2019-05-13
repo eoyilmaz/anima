458c1380e0ef98eccda4d0591a31d549a127864a
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-05-13 08:51:38 +0300

    * [Houdini] Updated the wrong copyright statement from the Toolbox script.

--- anima/env/houdini/toolbox.py
+++ anima/env/houdini/toolbox.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright (c) 2012-2018, Anima Istanbul
+# Copyright (c) 2018-2019, Erkan Ozgur Yilmaz
 #
 # This module is part of anima-tools and is released under the BSD 2
 # License: http://www.opensource.org/licenses/BSD-2-Clause
