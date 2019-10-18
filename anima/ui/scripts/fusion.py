354903b4cfac3c5ba867f3f7f42c0c1504f0d7ee
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-10-18 13:30:43 +0300

    * [UI] Updated ui script for Fusion which allows it to be called properly from another Qt ui like the Fusion toolbox.

--- anima/ui/scripts/fusion.py
+++ anima/ui/scripts/fusion.py
@@ -6,10 +6,9 @@
 import logging
 
 from anima import logger
-from anima.utils import do_db_setup
 
 
-def version_creator(lib='PySide', logging_level=logging.WARNING):
+def version_creator(lib='PySide', logging_level=logging.WARNING, parent=None):
     """Helper function for version_creator UI for Fusion
 
     It uses with PySide by default you can opt to use PyQt4 instead by setting
@@ -20,6 +19,7 @@ def version_creator(lib='PySide', logging_level=logging.WARNING):
     :return: None
     """
     # connect to db
+    from anima.utils import do_db_setup
     do_db_setup()
 
     from anima.ui import SET_PYSIDE, SET_PYQT4
@@ -35,4 +35,4 @@ def version_creator(lib='PySide', logging_level=logging.WARNING):
 
     from anima.ui import version_creator
     logger.setLevel(logging_level)
-    version_creator.UI(environment=fusion_env)
+    version_creator.UI(environment=fusion_env, parent=parent)
