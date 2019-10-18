d797e2fb0cea5369b22c6ea4b86a0ce164f55251
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2019-10-18 13:34:39 +0300

    * [BlackMagic] Cleaned up the code coming from BlackMagic that allows ``anima`` to be connected to DaVinci Resolve.

--- anima/env/blackmagic.py
+++ anima/env/blackmagic.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 
@@ -5,38 +6,51 @@ def get_bmd():
     """this routine is directly from BlackMagic
     """
     try:
-        # The PYTHONPATH needs to be set correctly for this import statement to work.
-        # An alternative is to import the DaVinciResolveScript by specifying absolute path (see ExceptionHandler logic)
+        # The PYTHONPATH needs to be set correctly for this import statement to
+        # work.
+        # An alternative is to import the DaVinciResolveScript by specifying
+        # absolute path (see ExceptionHandler logic)
         import DaVinciResolveScript as bmd
     except ImportError:
         expected_path = ''
         if sys.platform.startswith("darwin"):
             expected_path = \
-                "/Library/Application Support/Blackmagic Design/DaVinci Resolve/Developer/Scripting/Modules/"
-        elif sys.platform.startswith("win") or sys.platform.startswith("cygwin"):
-            import os
+                "/Library/Application Support/Blackmagic Design/DaVinci " \
+                "Resolve/Developer/Scripting/Modules/"
+        elif sys.platform.startswith("win") or \
+                sys.platform.startswith("cygwin"):
             expected_path = os.path.normpath(
                 os.path.join(
                     os.environ['PROGRAMDATA'],
-                    "Blackmagic Design/DaVinci Resolve/Support/Developer/Scripting/Modules/"
+                    "Blackmagic Design/DaVinci Resolve/Support/Developer/"
+                    "Scripting/Modules/"
                 )
             )
         elif sys.platform.startswith("linux"):
             expected_path = "/opt/resolve/libs/Fusion/Modules/"
 
         # check if the default path has it...
-        print("Unable to find module DaVinciResolveScript from $PYTHONPATH - trying default locations")
+        print(
+            "Unable to find module DaVinciResolveScript from $PYTHONPATH - "
+            "trying default locations"
+        )
         try:
             import imp
             print('Trying to import DaVinciResolveScript!')
             print('expected_path: %s' % expected_path)
-            bmd = imp.load_source('DaVinciResolveScript', expected_path + "DaVinciResolveScript.py")
+            bmd = imp.load_source(
+                'DaVinciResolveScript',
+                os.path.join(
+                    expected_path, "DaVinciResolveScript.py"
+                )
+            )
         except ImportError:
             # No fallbacks ... report error:
-            print("Unable to find module DaVinciResolveScript - please ensure that the module "
-                  "DaVinciResolveScript is discoverable by python")
-            print("For a default DaVinci Resolve installation, the module is expected to be "
-                  "located in: %s" % expected_path)
+            print("Unable to find module DaVinciResolveScript - please "
+                  "ensure that the module DaVinciResolveScript is "
+                  "discoverable by python")
+            print("For a default DaVinci Resolve installation, the module "
+                  "is expected to be located in: %s" % expected_path)
             sys.exit()
 
     return bmd
