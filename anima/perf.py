a579159f6b699bba15ecd1603d7a1a1999dd1769
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2020-01-30 00:13:47 +0300

    * [Perf] Added new utility class for measuring code execution duration.

--- anima/perf.py
+++ anima/perf.py
@@ -42,3 +42,55 @@ def measure_time(f_name):
 
         return wrapped_f
     return wrapper
+
+
+class TimeReporter(object):
+    """A utility class to easily measure time and report it in a consistent
+    manner without the repetition of boiler plate code.
+
+    Usage:
+        reporter = TimeReporter()
+        reporter.start("Measuring something")
+        # .
+        # .
+        # do something that takes some processing time
+        reporter.report()  # This will print the measured time
+
+        # or you can use
+        reporter.measure()  # To measure something without reporting it
+
+    """
+
+    def __init__(self):
+        self._title = None
+        self.report_text = ""
+        self.time_format = '%0.2f sec'
+        self.title = 'Process'
+
+        self.start_time = 0
+        self.end_time = 0
+        self.duration = 0
+
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, title):
+        self._title = title
+        self.report_text = "%s: %s" % (self.time_format, self.title)
+
+    def start(self, title):
+        self.title = title
+        self.start_time = time.time()
+
+    def measure(self):
+        self.end_time = time.time()
+        self.duration += self.end_time - self.start_time
+
+        # reset start time
+        self.start_time = time.time()
+
+    def report(self):
+        self.measure()
+        print(self.report_text % self.duration)
