114622e8c343f000b975a5b7e1c7f7d172bc03d8
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2020-06-28 14:03:07 +0300

    * [UI] Fixed ``Version Creator UI`` for Python 3.

--- anima/ui/version_creator.py
+++ anima/ui/version_creator.py
@@ -970,9 +970,11 @@ class MainDialog(QtWidgets.QDialog, AnimaDialogBase):
                             "tasks:<br><br>%s<br><br>"
                             "So, you can not un-publish it!" %
                             "<br>".join(
-                                map(
-                                    lambda x: x.name,
-                                    related_tasks
+                                list(
+                                    map(
+                                        lambda x: x.name,
+                                        related_tasks
+                                    )
                                 )
                             )
                         )
@@ -1004,9 +1006,11 @@ class MainDialog(QtWidgets.QDialog, AnimaDialogBase):
                             "tasks:<br><br>%s<br><br>"
                             "So, you can not delete it!" %
                             "<br>".join(
-                                map(
-                                    lambda x: x.name,
-                                    related_tasks
+                                list(
+                                    map(
+                                        lambda x: x.name,
+                                        related_tasks
+                                    )
                                 )
                             )
                         )
@@ -1330,7 +1334,7 @@ class MainDialog(QtWidgets.QDialog, AnimaDialogBase):
                     .execute(text(sql), task_id=task_id)\
                     .fetchall()
 
-                takes = map(lambda x: x[0], result)
+                takes = list(map(lambda x: x[0], result))
 
                 if not self.repr_as_separate_takes_checkBox.isChecked():
                     # filter representations
@@ -1616,7 +1620,7 @@ class MainDialog(QtWidgets.QDialog, AnimaDialogBase):
 
         data_from_db = \
             query.order_by(Version.version_number.desc()).limit(count).all()
-        versions = map(lambda x: VersionNT(*x), data_from_db)
+        versions = list(map(lambda x: VersionNT(*x), data_from_db))
         versions.reverse()
 
         self.previous_versions_table_widget.update_content(versions)
