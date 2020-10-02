0969068867d8fd8d5c11bd0135f634351fae71c2
arclightstudio <62157632+arclightstudio@users.noreply.github.com>
2020-10-02 09:57:33 +0300

    Create user_dialog.py

---
+++ anima/ui/user_dialog.py
@@ -0,0 +1,167 @@
+from PySide2 import QtWidgets
+from PySide2 import QtCore
+from shiboken2 import wrapInstance
+
+from anima.ui.base import AnimaDialogBase, ui_caller
+
+import maya.OpenMayaUI as omui
+
+
+
+def UI(app_in=None, executor=None, **kwargs):
+    """
+    :param app_in: A Qt Application instance, which you can pass to let the UI
+      be attached to the given applications event process.
+
+    :param executor: Instead of calling app.exec_ the UI will call this given
+      function. It also passes the created app instance to this executor.
+
+    """
+    user_dialog=UserDialog()
+    try:
+        user_dialog.close()
+        user.dialog.deleteLater()
+    except:
+        pass
+
+    return ui_caller(app_in, executor, UserDialog, **kwargs)
+    
+
+    
+def maya_main_window():
+    """
+    Return the Maya main window widget as a Python object
+    """
+    
+    main_window_ptr=omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window_ptr),QtWidgets.QWidget)
+    
+
+class UserDialog(QtWidgets.QDialog):
+    """
+    The User Creation Dialog
+    """
+    def __init__(self,parent=maya_main_window()):
+        super(UserDialog,self).__init__(parent)
+        
+        self.setWindowTitle("User Dialog")
+        self.setWindowFlags(self.windowFlags()^QtCore.Qt.WindowContextHelpButtonHint)
+        
+        
+        self.create_widgets()
+        self.create_layouts()
+        self.create_connections()
+        
+    def create_widgets(self):
+        
+        self.dialog_label=QtWidgets.QLabel()
+        self.dialog_label.setStyleSheet("color: rgb(71, 143, 202);\n"
+                                   "font: 18pt;")        
+        self.dialog_label.setText("Create User")
+        
+        self.line = QtWidgets.QFrame()
+        self.line.setFrameShape(QtWidgets.QFrame.HLine)
+        self.line.setFrameShadow(QtWidgets.QFrame.Sunken)
+        
+        self.name_line_edit=QtWidgets.QLineEdit()
+        self.name_line_edit.setPlaceholderText("Enter Name")
+        
+        self.login_line_edit=QtWidgets.QLineEdit()
+        self.login_line_edit.setPlaceholderText("stalker")
+        
+        self.email_line_edit=QtWidgets.QLineEdit()
+        self.email_line_edit.setPlaceholderText("stalker@gmail.com")
+        
+        self.password_line_edit=QtWidgets.QLineEdit()
+        self.password_line_edit.setPlaceholderText("******")
+        
+        self.ok_button=QtWidgets.QPushButton("OK")
+        self.cancel_button=QtWidgets.QPushButton("Cancel")
+               
+       
+        
+    def create_layouts(self):
+
+        form_layout=QtWidgets.QFormLayout()
+        form_layout.addRow("Name", self.name_line_edit)
+        form_layout.addRow("Login", self.login_line_edit)
+        form_layout.addRow("Email", self.email_line_edit)
+        form_layout.addRow("Password", self.password_line_edit)
+        
+        button_layout=QtWidgets.QHBoxLayout()
+        button_layout.addStretch()
+        button_layout.addWidget(self.ok_button)
+        button_layout.addWidget(self.cancel_button)
+        
+        label_layout=QtWidgets.QVBoxLayout()
+        label_layout.addWidget(self.dialog_label)
+        label_layout.addWidget(self.line)
+        label_layout.addStretch()
+        
+        main_layout=QtWidgets.QVBoxLayout(self)
+        main_layout.addLayout(label_layout)
+        main_layout.addLayout(form_layout)
+        main_layout.addLayout(button_layout)
+        
+
+        self.resize(328,184)
+        
+        
+    def create_connections(self):
+        
+        self.ok_button.clicked.connect(self.check_userPass)
+        self.cancel_button.clicked.connect(self.close)
+        
+        
+    def check_userPass(self):
+        
+        self.user_name=self.name_line_edit.text()
+        self.user_login=self.login_line_edit.text()
+        self.user_email=self.email_line_edit.text()
+        self.user_password=self.password_line_edit.text()
+        
+        if len(self.user_name)<2 or len(self.user_login)<2 or len(self.user_email)<2 or len(self.user_password)<2 :
+            msg = QtWidgets.QMessageBox()
+            msg.setIcon(QtWidgets.QMessageBox.Information)
+
+            msg.setText("Please, fill out user information completely!")
+            msg.setWindowTitle("Information Message")
+            msg.setStandardButtons(QtWidgets.QMessageBox.Ok) 
+            msg.exec_() 
+            
+        else:
+            try:
+                self.create_user()
+            except:
+                pass
+                
+            self.close()
+            msg2 = QtWidgets.QMessageBox()
+            msg2.setIcon(QtWidgets.QMessageBox.Information)
+
+            msg2.setText("User '{0}' successfully created!".format(self.user_login))
+            msg2.setWindowTitle("Information Message")
+            msg2.setStandardButtons(QtWidgets.QMessageBox.Ok)
+            msg2.exec_()
+
+            
+   
+    def create_user(self):
+
+        from stalker.db.session import DBSession
+        from stalker import User
+        me = User(
+            name="{0}".format(self.user_name),
+            login="{0}".format(self.user_login),
+            email="{0}".format(self.user_email),
+            password="{0}".format(self.user_password)
+        )
+        
+        # Save the user to database
+        DBSession.save(me)
+        
+ 
+        
+        
+
+        
