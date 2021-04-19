0403d916ecde99ca7a5c2ca4358fec4e45b4bd63
Mehmet Erer <erer.mt@gmail.com>
2021-04-19 15:36:42 +0300

    * [Resolve] Hardcoded paths removed from get_timecode_from_image() for exıftool.

--- anima/env/resolve/conformer.py
+++ anima/env/resolve/conformer.py
@@ -633,39 +633,13 @@ class MainDialog(QtWidgets.QDialog, AnimaDialogBase):
     def get_timecode_from_image(self, fps, img_path):
         """queries timecode metadata from given image
         """
-        import os
-        import platform
         import subprocess
         import timecode
-
-        os_name = 'nt'
-        if os.name == 'posix':
-            if platform.system() == 'Linux':
-                os_name = 'posix'
-            elif platform.system() == 'Darwin':
-                os_name = 'darwin'
-
-        exif_path = None
-        if os_name == 'nt':
-            dev_path = 'T:/DEV'
-            exif_path = os.path.join(dev_path, 'lib/windows/py2/__extras__/exiftool.exe')
-            exif_path = os.path.normpath(exif_path).replace('\\', '/')
-        elif os_name == 'darwin':
-            dev_path = '/Volumes/GFX/DEV'
-            exif_path = os.path.join(dev_path, 'lib/osx/py2/__extras__/exiftool')
-            exif_path = os.path.normpath(exif_path).replace('\\', '/')
-        elif os_name == 'linux':
-            dev_path = '/mnt/T/DEV'
-            exif_path = os.path.join(dev_path, 'lib/linux/py2/__extras__/exiftool')
-            exif_path = os.path.normpath(exif_path).replace('\\', '/')
-
-        if not os.path.exists(exif_path):
-            raise RuntimeError('ExifTool path does not exist!.')
   
         frame_number = 0
 
         info = {}
-        process = subprocess.Popen([exif_path, img_path],
+        process = subprocess.Popen(['exiftool', img_path],
                                    stdout=subprocess.PIPE,
                                    stderr=subprocess.STDOUT,
                                    universal_newlines=True)
