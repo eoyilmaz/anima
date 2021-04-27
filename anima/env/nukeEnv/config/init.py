e74d41ed48cb5feb187f8b84c54b93aba9b9404f
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2021-04-27 16:05:29 +0300

    * [Nuke] Fixed ``nukeEnv/config/init.py`` for Windows.

--- anima/env/nukeEnv/config/init.py
+++ anima/env/nukeEnv/config/init.py
@@ -9,7 +9,10 @@ utils.do_db_setup()
 
 # iterate over environment and set it in TCL
 for key, value in os.environ.iteritems():
-    nuke.tcl("set", str(key), str(value))
+    try:
+        nuke.tcl("set", str(key), str(value))
+    except RuntimeError:
+        pass
 
 
 def filter_env_vars_in_filepath(filename):
