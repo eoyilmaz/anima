83c7b7539bd35de2a044b52029138178036db72b
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-06-09 10:11:24 +0100

    * [UI] Added icon for ``blender-3.2``. * [Rez] Added version ``3.2.0`` for ``blender`` package.

---
+++ anima/rez_packages/blender/3.2.0/package.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+name = "blender"
+
+version = "3.2.0"
+
+author = [
+    "Erkan Ozgur Yilmaz",
+]
+
+uuid = "edf2e1216f1744e286dbfc5becd985e5"
+
+description = "Blender Package"
+
+requires = [
+    "aces",
+    "python-3.10",
+    "pyside2",
+]
+
+build_command = "python {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    env.PATH.prepend("{root}/bin")
