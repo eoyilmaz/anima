f1d96751a1f47674592a8327534d91af5a5c2e86
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-11-25 16:01:09 +0000

    - [Rez] Updated ``blender`` packages and removed the part that sets the ``PYTHONHOME`` which is breaking other packages that is requiring ``blender`` (i.e. ``anima``) not to build.

--- anima/rez_packages/blender/3.2.2/package.py
+++ anima/rez_packages/blender/3.2.2/package.py
@@ -26,16 +26,3 @@ build_command = "python3 {root}/../build.py {install}"
 def commands():
     # env.PYTHONPATH.append("{root}/python")
     env.PATH.prepend("{root}/bin")
-
-    major = env.REZ_BLENDER_MAJOR_VERSION,
-    minor = env.REZ_BLENDER_MINOR_VERSION,
-    patch = env.REZ_BLENDER_PATCH_VERSION,
-    if system.platform == "osx":
-        # env.PATH.append(
-        #     f"/Applications/Blender-{major}.{minor}.{patch}.app/Contents/MacOS"
-        # )
-        env.PYTHONHOME.set(
-            f"/Applications/Blender-{major}.{minor}.{patch}.app/Contents/"
-            f"Resources/{major}.{minor}/python"
-        )
-
