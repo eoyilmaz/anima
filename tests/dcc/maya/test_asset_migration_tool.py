f0a9d879ccb73a1e007e5475636e81d648a43208
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-12-11 15:41:12 +0000

    - [Maya] Modernised the ``RenderSlicer`` tests.

--- tests/dcc/maya/test_asset_migration_tool.py
+++ tests/dcc/maya/test_asset_migration_tool.py
@@ -3,17 +3,41 @@
 import pytest
 
 
-def test_asset_migration_tool_initialization(create_test_db, create_project):
-    """Test AssetMigrationTool initialization."""
-    from stalker import Asset, Task, Project
-    project1 = create_project
-    project2 = Project.query.filter(Project.code=="TP2").first()
-    assert project2 is not None
-    env1 = Asset.query.filter(Asset.code=="Env1").first()
-    assert env1 is not None
-    assert env1.project == project1
-    # migrate the env1 to the second project
+@pytest.fixture(scope="function")
+def migration_test_data(create_test_data, create_pymel, create_maya_env):
+    data = create_test_data
+    pm = create_pymel
+    maya_env = create_maya_env
 
+    # *********************************
+    # Asset2
+    # *********************************
+
+    # Asset2 - Take1
+    pm.newFile(force=True)
+    root_node = pm.nt.Transform(name='Asset2_Main')
+    box = pm.polyCube(name="Box1")[0]
+    pm.parent(box, root_node)
+    pm.runtime.DeleteHistory()
+    maya_env.save_as(data["version1"])
+    maya_env.save_as(data["version2"])
+    maya_env.save_as(data["version3"])
+
+    # Asset2 - Take1
+    pm.newFile(force=True)
+    root_node = pm.nt.Transform(name='Asset2_Take1')
+    box = pm.polyCube(name="Box1")[0]
+    pm.parent(box, root_node)
+    pm.runtime.DeleteHistory()
+    maya_env.save_as(data["version4"])
+    maya_env.save_as(data["version5"])
+    maya_env.save_as(data["version6"])
+
+    yield data
+
+
+def test_asset_migration_tool_initialization(migration_test_data):
+    """Test AssetMigrationTool initialization."""
     from anima.dcc.mayaEnv.general import AssetMigrationTool
     amt = AssetMigrationTool()
     assert isinstance(amt, AssetMigrationTool)
