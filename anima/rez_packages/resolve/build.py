bc80a62e18a28aa8b44abfee2ddba807b8a3e816
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2022-12-14 16:42:07 +0000

    - [Rez] Introduced a new function to the build script of ``resolve`` package that fixes shebangs for resolve shell scripts according to the OS.

--- anima/rez_packages/resolve/build.py
+++ anima/rez_packages/resolve/build.py
@@ -1,8 +1,12 @@
 #!/usr/bin/env python
 import os
+import re
 import shutil
 import sys
 import pathlib
+
+
+shebang_regex = re.compile("(#!\{shebang)(\.)([\w]+)(})")
 
 
 def build(source_path, build_path, install_path, targets):
@@ -11,14 +15,58 @@ def build(source_path, build_path, install_path, targets):
     print(f"install_path: {install_path}")
     print(f"targets     : {targets}")
 
+    source_path = pathlib.Path(source_path)
+    install_path = pathlib.Path(install_path)
+
     # copy the bin folder content to the install_path
     for dir in ["bin"]:
-        shutil.rmtree(pathlib.Path(install_path) / dir, ignore_errors=True)
+        shutil.rmtree(install_path / dir, ignore_errors=True)
         os.makedirs(install_path, exist_ok=True)
         shutil.copytree(
-            pathlib.Path(source_path) / ".." / dir,
-            pathlib.Path(install_path) / dir,
+            source_path / ".." / dir,
+            install_path / dir,
         )
+
+    # Fix shebangs
+    for file_path in (install_path / dir).glob("*"):
+        fix_shebang(file_path)
+
+
+def fix_shebang(file_path):
+    """Fix the shebang of the given file according to the current os.
+
+    Args:
+        file_path (Union[str, Path]: Path of the file to be fixed.
+    """
+    shebangs = {
+        "win32": None,
+        "linux": {
+            "sh": "/usr/bin/sh"
+        },
+        "darwin": {
+            "sh": "/bin/sh"
+        },
+    }
+    if sys.platform == "win32":
+        return
+
+    with open(file_path, "r") as f:
+        data = f.readlines()
+
+    # fix the shebang at the first line
+    match = shebang_regex.match(data[0])
+    if not match:
+        return
+
+    exec = match.groups()[2]
+    exec_path = shebangs[sys.platform].get(exec)
+    if not exec_path:
+        return
+
+    data[0] = "#!{}".format(exec_path)
+
+    with open(file_path, "w") as f:
+        f.writelines(data)
 
 
 if __name__ == "__main__":
