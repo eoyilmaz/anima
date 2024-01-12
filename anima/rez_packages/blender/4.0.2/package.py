edd1a6eed2bd0801eb69f67412fb4f4a9e6326c2
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-01-12 23:12:15 +0000

    - [Rez] Added Blender `4.0.0`, and `4.0.2` versions.

---
+++ anima/rez_packages/blender/4.0.2/package.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+name = "blender"
+
+version = "4.0.2"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "edf2e1216f1744e286dbfc5becd985e5"
+
+description = "Blender Package"
+
+requires = [
+    "aces",
+    ".python-3.10",
+]
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    env.PATH.prepend("{root}/bin")
