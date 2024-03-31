7d81b4a2835a69e6faeaab1b319e2454c9468c37
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-03-31 09:31:37 +0100

    - [REZ] Added Blender-4.1.0

---
+++ anima/rez_packages/blender/4.1.0/package.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+name = "blender"
+
+version = "4.1.0"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "edf2e1216f1744e286dbfc5becd985e5"
+
+description = "Blender Package"
+
+requires = [
+    "aces",
+    ".python-3.11",
+]
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    env.PATH.prepend("{root}/bin")
