13f333be3c5403507f29804c8d442be0b1cdb18f
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-12-18 13:26:19 +0000

    [REZ] Added `ocio-1.0.0` and `ocio-2.0.0` packages for backward compatibility.

---
+++ anima/rez_packages/ocio/1.0.0/package.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+name = "ocio"
+
+version = "1.0.0"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "81bb79d3c0744ea49d5bb4a21b3cb4e5"
+
+description = "OCIO Package"
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    # env.PATH.append("{root}/bin")
+    pass
