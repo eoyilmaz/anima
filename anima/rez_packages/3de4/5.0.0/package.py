c0b619ab475270ef89e4ed13ca3a5657652000b1
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2024-12-18 16:22:58 +0000

    [#47] Updated rez packages for `3de4` to set the `QT_API` environment variable.

--- anima/rez_packages/3de4/5.0.0/package.py
+++ anima/rez_packages/3de4/5.0.0/package.py
@@ -23,10 +23,13 @@ def commands():
     minor = env.REZ_3DE4_MINOR_VERSION
     if system.platform == "linux":
         env.TDE4_ROOT = f"/opt/3DE4_r{major}{minor}"
+        env.QT_API = "pyside2"
     elif system.platform == "osx":
         env.TDE4_ROOT = f"/Applications/3DE4_r{major}{minor}/Contents/MacOS/3DE4"
+        env.QT_API = "pyside6"
     elif system.platform == "windows":
         env.TDE4_ROOT = f"C:/Program Files/3DE4_r{major}{minor}"
+        env.QT_API = "pyside2"
     env.PATH.append("${TDE4_ROOT}/bin")
     env.PYTHONPATH.append("{root}/lib/python")
     env.PYTHON_CUSTOM_SCRIPTS_3DE4.append("{root}/lib/python")
