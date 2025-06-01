f05940085886266a626240e420998c420ca6eb54
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2025-06-01 22:49:36 +0100

    [#46] Added `blender-4.4.3` rez package.

---
+++ src/anima/rez_packages/blender/4.4.3/package.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+name = "blender"
+
+version = "4.4.3"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "edf2e1216f1744e286dbfc5becd985e5"
+
+description = "Blender Package"
+
+requires = [
+    "ocio-2.3",
+    "agx",
+    ".python-3.11",
+]
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    env.PATH.prepend("{root}/bin")
+
+    # set Qt API
+    env.QT_API = "pyside2"
