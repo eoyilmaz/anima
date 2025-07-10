7f16ac8dff4b55c43cd3cc8e4bb5d5b86fa562dd
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2025-07-10 15:52:47 +0100

    [#46] Added `fusion-20.0.1` rez package.

---
+++ src/anima/rez_packages/fusion/20.0.1/package.py
@@ -0,0 +1,35 @@
+
+name = "fusion"
+
+version = "20.0.1"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "59a8d4cd31414e888b4f859ef14b0fb7"
+
+description = "Fusion package"
+
+variants = [[".python-3"]]
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    # env.PATH.append("{root}/bin")
+
+    major = env.REZ_FUSION_MAJOR_VERSION
+    if system.platform == "linux":
+        env.PATH.append(f"/opt/BlackmagicDesign/Fusion{major}/")
+        env.PATH.append(f"/opt/BlackmagicDesign/FusionRenderNode{major}/")
+        env.PYTHONPATH.append(f"/opt/BlackmagicDesign/Fusion{major}/")
+    if system.platform == "osx":
+        env.PATH.append(
+            f"/Applications/Blackmagic Fusion {major}/Fusion.app/Contents/MacOS/"
+        )
+        env.PATH.append(
+            f"/Applications/Blackmagic Fusion {major} Render Node/Fusion Render Node.app/Contents/MacOS/"
+        )
+        env.PYTHONPATH.append(
+            f"/Applications/Blackmagic Fusion {major}/Fusion.app/Contents/MacOS/"
+        )
