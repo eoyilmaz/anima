869592dd61e04aeacfdaf757dbf79a2db3b33a99
Erkan Ozgur Yilmaz <eoyilmaz@gmail.com>
2026-07-18 10:32:04 +0100

    [REZ] Added Blender-5.2.0 package.

---
+++ src/anima/rez_packages/blender/5.2.0/package.py
@@ -0,0 +1,26 @@
+
+name = "blender"
+
+version = "5.2.0"
+
+author = ["Erkan Ozgur Yilmaz"]
+
+uuid = "edf2e1216f1744e286dbfc5becd985e5"
+
+description = "Blender Package"
+
+requires = [
+    "ocio-2.3",
+    "agx",
+    ".python-3.13",
+]
+
+build_command = "python3 {root}/../build.py {install}"
+
+
+def commands():
+    # env.PYTHONPATH.append("{root}/python")
+    env.PATH.prepend("{root}/bin")
+
+    # set Qt API
+    env.QT_API = "pyside6"
